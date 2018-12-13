82bbd602cab29ed80c8bccd1da6cb97fe7182c43
李留住 <liliuzhu@renrenche.com>
2018-12-13 12:15:32 +0800

    feat: 小程序调试webview

--- personalTest/pages/index/index.js
+++ personalTest/pages/index/index.js
@@ -1,7 +1,6 @@
 let app = getApp();
 var param = {
 	data: {
-		hasUserInfo: false,
 		userInfo: {
 			nickName:"Hello MiniProgram"
 		},
@@ -23,13 +22,19 @@ var param = {
 				name: '音乐播放器',
 				path: "../audio/pages/index/index",
 				logo: "../../static/baidu.jpg"
-			},
+      },
 			{
 				id: '04',
 				name: '背景音乐播放器',
 				path: "../audio/pages/backgroundAudio/backgroundAudio",
 				logo: "../../static/baidu.jpg"
-			}
+			},
+      {
+        id: '05',
+        name: 'webView',
+        path: "../webView/index?webViewUrl=https://busi.rrcimg.com/owner-pay-front-end/files-cdn/sellingfast1percent-v1-20181127.html",
+        logo: "../../static/baidu.jpg"
+      }
 		]
 	},
 	onLoad(option) {
@@ -38,20 +43,25 @@ var param = {
 	getUserInfo: function () {	
 		if (app.globalData.userInfo) {
 			this.setData({
-				userInfo: app.globalData.userInfo,
-				hasUserInfo: true
+				userInfo: app.globalData.userInfo
 			})
 		} else {
 			// 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
 			// 所以此处加入 callback 以防止这种情况
 			app.userInfoReadyCallback = res => {
 				this.setData({
-					userInfo: res.userInfo,
-					hasUserInfo: true
+					userInfo: res.userInfo
 				})
 			}
 		}
 	},
+  onGotUserInfo: function (e) {
+    // console.log(e.detail.errMsg)
+    // console.log(e.detail.userInfo)
+    // console.log(e.detail.rawData)
+    app.globalData.userInfo = e.detail.userInfo
+    this.getUserInfo();
+  },
 	navigateTo(e) {
 		let path = e.currentTarget.dataset.path;
 		wx.navigateTo({
